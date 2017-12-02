813d77c3425c8c6e7d81d774fa9c9a5c53282f51
Edward Gargan <edwgargan@gmail.com>
2017-12-02 16:34:58 +0000

    Created intial classes as per cw spec.

---
+++ app/src/main/java/com/egargan/recipebook/RecipeViewActivity.java
@@ -0,0 +1,15 @@
+package com.egargan.recipebook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RecipeViewActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_view);
+    }
+
+
+}
